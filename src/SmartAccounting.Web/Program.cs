3d61ebd3d3126d7ba4685728056194ab2534a849
Marcelo.Souza <33257503+mgdesouza@users.noreply.github.com>
2026-09-04 14:58:56 -0300

    feat: configure EF Core SQL Server and ASP.NET Core Identity

--- src/SmartAccounting.Web/Program.cs
+++ src/SmartAccounting.Web/Program.cs
@@ -1,6 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SmartAccounting.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 8;
+})
+.AddRoles<IdentityRole<int>>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Dashboard");
+});
 
 var app = builder.Build();
 
@@ -13,6 +38,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapRazorPages();
 
