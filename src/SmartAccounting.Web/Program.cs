f8dbd6e7ab809bcb40bf891be4a2f344c7aac2cf
Marcelo.Souza <33257503+mgdesouza@users.noreply.github.com>
2026-09-04 15:01:23 -0300

    feat: configure Identity roles and permission policies

--- src/SmartAccounting.Web/Program.cs
+++ src/SmartAccounting.Web/Program.cs
@@ -22,10 +22,19 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 .AddRoles<IdentityRole<int>>()
 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.AddRazorPages(options =>
+builder.Services.AddAuthorization(options =>
 {
-    options.Conventions.AuthorizeFolder("/Dashboard");
+    options.AddPolicy("Dashboard.View", policy => policy.RequireClaim("permission", "Dashboard.View"));
+    options.AddPolicy("Empresa.View", policy => policy.RequireClaim("permission", "Empresa.View"));
+    options.AddPolicy("Empresa.Manage", policy => policy.RequireClaim("permission", "Empresa.Manage"));
+    options.AddPolicy("Ecd.View", policy => policy.RequireClaim("permission", "Ecd.View"));
+    options.AddPolicy("Ecd.Import", policy => policy.RequireClaim("permission", "Ecd.Import"));
+    options.AddPolicy("Contabilidade.View", policy => policy.RequireClaim("permission", "Contabilidade.View"));
+    options.AddPolicy("Relatorio.View", policy => policy.RequireClaim("permission", "Relatorio.View"));
+    options.AddPolicy("Usuario.Manage", policy => policy.RequireClaim("permission", "Usuario.Manage"));
 });
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -40,6 +49,14 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<int>>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    await IdentitySeeder.SeedAsync(roleManager, userManager);
+}
+
 app.MapRazorPages();
 
 app.Run();
